da180960294d94fc2d9f00d164519aaf27cf87f2
JaysusM <jmartinruiz@uma.es>
2019-12-21 00:40:51 +0100

    :tada: Add project base

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Landing } from './components/Landing';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
